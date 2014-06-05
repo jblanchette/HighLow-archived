f2c85c3f5e1560571b00ecdd5b0649b410f0d0b6
jblanchette <jeff.blanchette5@gmail.com>
2014-06-05 13:36:41 -0400

    Added basic file structure.  Can now start the function/object skeletons.

--- Server/server.v2.js
+++ Server/server.v2.js
@@ -2,5 +2,18 @@ var io = require('socket.io').listen(8080, {log: false}),
 _ = require('underscore');
 
 var Server = {
+    init: function(){
 
+    },
+
+    setup: function(){
+
+    },
+
+    handleConnection: function(){
+
+    }
 };
+
+Server.init();
+io.sockets.on("connection", Server.handleConnection);
