641ed5765df1b15ecbc9c6f4a88f552f48cdec53
jblanchette <jeff.blanchette5@gmail.com>
2014-06-05 16:34:31 -0400

    Changed structure again.

---
+++ Server/lib/controllers/message/MessageConfig.js
@@ -0,0 +1,16 @@
+var msgConfig = {
+    "LOGIN": {
+        "Login": "login/Login",
+        "Logout": "login/Logout",
+        "Register": "login/Register",
+        "UpdateInfo": "log/UpdateInfo"
+    },
+
+    "CHAT": {
+        "Join": "chat/Join",
+        "Leave": "chat/Leave",
+        "Make": "chat/Make",
+        "NewMsg": "chat/NewMsg",
+        "SpecialFunc": "chat/SpecialFunc"
+    }
+};
